3819eba66cfee89a8d4d670de248bfce12bfdde3
enric.ruizmartinez <enric.ruizmartinez@38b21a9a-99dd-11dd-93a9-677c3c677ea2>
2008-10-28 15:29:47 +0000

    New functions added to IProgress

--- trunk/dotnet-vboxws/IProgress.cs
+++ trunk/dotnet-vboxws/IProgress.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Web.Services.Protocols;
 
+// Next -> getId
+
 namespace VBoxWebService
 {
     public class IProgress : VBoxInterface
@@ -9,11 +11,11 @@ namespace VBoxWebService
         {
         }
 
-        public void waitForCompletion(int timeout)
+        public void cancel()
         {
             try
             {
-                access.IProgress_waitForCompletion(this._this, timeout);
+                this.access.IProgress_cancel(this._this);
             }
             catch (SoapException ex)
             {
@@ -21,11 +23,71 @@ namespace VBoxWebService
             }
         }
 
-        public long getResultCode()
+        public void waitForCompletion(int timeout)
         {
             try
             {
-                return access.IProgress_getResultCode(this._this);
+                this.access.IProgress_waitForCompletion(this._this, timeout);
+            }
+            catch (SoapException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string description()
+        {
+            try
+            {
+                return this.access.IProgress_getDescription(this._this);
+            }
+            catch (SoapException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool completed()
+        {
+            try
+            {
+                return this.access.IProgress_getCompleted(this._this);
+            }
+            catch (SoapException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool canceled()
+        {
+            try
+            {
+                return this.access.IProgress_getCanceled(this._this);
+            }
+            catch (SoapException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool cancelable()
+        {
+            try
+            {
+                return this.access.IProgress_getCancelable(this._this);
+            }
+            catch (SoapException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public long resultCode()
+        {
+            try
+            {
+                return this.access.IProgress_getResultCode(this._this);
             }
             catch (SoapException ex)
             {
